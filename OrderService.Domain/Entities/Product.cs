af323d3ce60cafa1844cc0c3b82ef5173ea697df
Эдик <@DESKTOP-JF7JLNV>
2022-04-12 00:14:36 +0300

    Domain: added Product, Category entities

---
+++ OrderService.Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using OrderService.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderService.Domain.Entities
+{
+    public class Product : IdentifiableEntity
+    {
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ProductCategoryId { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public int? UnitsInStock { get; set; }
+        public int? UnitsInOrder { get; set; }
+        public byte[] Image { get; set; }
+        public bool Active { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+
+        public ICollection<OrderDetail> OrderDetails { get; private set; }
+    }
+}
---
+++ OrderService.Domain/Entities/ProductCategory.cs
@@ -0,0 +1,21 @@
+﻿using OrderService.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderService.Domain.Entities
+{
+    public class ProductCategory : IdentifiableEntity
+    {
+        public ProductCategory()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+
+        public ICollection<Product> Products { get; private set; }
+    }
+}
