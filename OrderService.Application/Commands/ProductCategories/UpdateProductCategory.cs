f64d1ed88595b4d38f37e485fb41aa75e16e0a61
Эдик <@DESKTOP-JF7JLNV>
2022-04-17 20:49:25 +0300

    Added Application project

---
+++ OrderService.Application/Commands/ProductCategories/UpdateProductCategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderService.Application.Commands.ProductCategories
+{
+    public class UpdateProductCategory
+    {
+
+    }
+}
